1c3892f424043f7105f6c91c0b57a2c595f50cad
Vinícius Müller <vinigm.nho@gmail.com>
2023-05-24 03:21:26 -0300

    Move code to separate crates and use templates

---
+++ pageturtle_cli/src/main.rs
@@ -0,0 +1,130 @@
+use std::{
+    fs,
+    path::{Path, PathBuf}, rc::Rc, borrow::Borrow
+};
+
+use comrak::{
+    plugins::syntect::SyntectAdapter, Arena, ComrakExtensionOptions, ComrakOptions, ComrakPlugins,
+};
+use pageturtle_core::{
+    build_blog_post, prepare_for_publish, BlogPost, PostCompiler, PublishableBlogPost, render_index,
+};
+use walkdir::WalkDir;
+
+#[derive(Debug)]
+/// Error that can happen when building a post from a filepath.
+/// Contains OS-level metadata such as filepath or file content.
+struct BuildPostError {
+    filepath: PathBuf,
+    content: String,
+    line: u32,
+    column: u32,
+    message: String,
+}
+
+fn main() {
+    // The returned nodes are created in the supplied Arena, and are bound by its lifetime.
+    let arena = Arena::new();
+
+    let adapter = SyntectAdapter::new("base16-ocean.dark");
+    let mut plugins = ComrakPlugins::default();
+    plugins.render.codefence_syntax_highlighter = Some(&adapter);
+    let options = &ComrakOptions {
+        extension: ComrakExtensionOptions {
+            front_matter_delimiter: Some("---".to_owned()),
+            ..ComrakExtensionOptions::default()
+        },
+        ..ComrakOptions::default()
+    };
+
+    let compiler = PostCompiler::new(arena, options, &plugins);
+
+    let allowed_filetypes = vec!["md", "markdown"];
+    let mut posts: Vec<BlogPost> = vec![];
+    let mut failures: Vec<BuildPostError> = vec![];
+    let blog_path = "blog_template/posts";
+    let output_target = "dist";
+
+    let walker = WalkDir::new(blog_path).min_depth(1).into_iter();
+    for entry in walker {
+        let entry = entry.unwrap();
+        if entry.file_type().is_dir() {
+            continue;
+        };
+
+        let filepath = entry.path();
+        match filepath.extension() {
+            Some(e) => {
+                if !allowed_filetypes.contains(&e.to_str().unwrap()) {
+                    continue;
+                }
+            }
+            None => continue,
+        }
+
+        let content = fs::read_to_string(&filepath).unwrap();
+
+        match build_blog_post(&content, &compiler) {
+            Ok(post) => posts.push(post),
+            Err(e) => failures.push(BuildPostError {
+                filepath: filepath.into(),
+                content,
+                line: e.line,
+                column: e.column,
+                message: e.message,
+            }),
+        };
+    }
+
+    let output_dir = Path::new(output_target);
+
+    if !output_dir.exists() {
+        fs::create_dir_all(output_dir).unwrap();
+    }
+
+    let publishable_posts: Vec<PublishableBlogPost> = posts
+        .iter()
+        .map(|p| prepare_for_publish(p, &compiler))
+        .collect();
+    // let arc_posts = Rc::new(publishable_posts);
+
+    // create_search_index(&publishable_posts).unwrap(); // TODO: handle (create error type for file
+                                                      // write error, etc...)
+
+    // create_index_page(output_dir, &publishable_posts);
+
+
+    // create index page
+    // let posts_iter = &boxed_posts.iter();
+    let index_html = render_index(&publishable_posts);
+    let path = output_dir.join("index.html");
+    fs::write(path, index_html).unwrap();
+
+    for post in &publishable_posts {
+        let path = output_dir.join(post.filename.to_owned());
+        println!("writing file {:?}", path);
+        fs::write(path, post.rendered_html.to_owned()).unwrap();
+    };
+
+    dbg!(failures);
+}
+
+fn create_search_index<'a, I>(_posts: &I) -> Result<(), String>
+where
+    I: Iterator<Item = PublishableBlogPost<'a>>,
+{
+    Ok(())
+}
+
+fn create_posts<'a, I>(output_dir: &Path, posts: &'a I) -> Result<(), String>
+where
+    I: Iterator<Item = PublishableBlogPost<'a>>,
+{
+    Ok(())
+}
+
+fn create_index_page<'a, I>(output_dir: &Path, posts: &'a I)
+where
+    I: Iterator<Item = PublishableBlogPost<'a>>,
+{
+}
---
+++ pageturtle_core/src/lib.rs
@@ -0,0 +1,214 @@
+// TODO: modularize
+// TODO: move core to another crate and leave this just as a CLI app
+use askama::Template; // bring trait in scope
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+use slug::slugify;
+use std::{
+    borrow::Borrow,
+    cell::RefCell,
+    path::{Path, PathBuf},
+};
+
+use comrak::plugins::syntect::SyntectAdapter;
+use comrak::{
+    arena_tree::Node,
+    nodes::{Ast, AstNode},
+    Arena, ComrakExtensionOptions, ComrakOptions, ComrakPlugins,
+};
+
+#[derive(Template)]
+#[template(path = "post.html", escape = "none")]
+struct PostTemplate<'a> {
+    // TODO: compose templates
+    base_url: &'a str,
+    title: &'a str,
+    content: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "index.html", escape = "none")]
+struct IndexTemplate<'a> {
+    // TODO: compose templates
+    base_url: &'a str,
+    title: &'a str,
+    posts: &'a Vec<PublishableBlogPost<'a>>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BlogPostMetadata {
+    title: String,
+    slug: Option<String>,
+    #[serde(with = "date")]
+    date: DateTime<Utc>,
+    tags: Option<Vec<String>>,
+}
+
+pub struct PostCompiler<'a> {
+    arena: Arena<AstNode<'a>>,
+    options: &'a ComrakOptions,
+    plugins: &'a ComrakPlugins<'a>,
+}
+
+impl<'a> PostCompiler<'a> {
+    pub fn new<'b>(
+        arena: Arena<AstNode<'a>>,
+        options: &'a ComrakOptions,
+        plugins: &'a ComrakPlugins<'a>,
+    ) -> PostCompiler<'a> {
+        Self {
+            arena,
+            options,
+            plugins,
+        }
+    }
+
+    pub fn to_ast(&'a self, content: &str) -> &'a AstNode<'a> {
+        comrak::parse_document(&self.arena, content, self.options)
+    }
+
+    pub fn ast_to_html(&'a self, ast: &'a AstNode<'a>) -> String {
+        let mut output_buffer = Vec::new();
+        comrak::format_html_with_plugins(ast, self.options, &mut output_buffer, self.plugins)
+            .unwrap();
+        String::from_utf8(output_buffer).unwrap()
+    }
+}
+
+mod date {
+    use chrono::{DateTime, TimeZone, Utc};
+    use serde::{self, Deserialize, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%SZ";
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT)
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+#[derive(Debug)]
+pub struct BlogPost<'a> {
+    pub metadata: BlogPostMetadata,
+    pub raw_content: String,
+    pub ast: &'a Node<'a, RefCell<Ast>>,
+}
+
+#[derive(Debug)]
+pub struct PublishableBlogPost<'a> {
+    pub post: &'a BlogPost<'a>,
+    pub filename: PathBuf,
+    pub rendered_html: String,
+}
+
+#[derive(Debug)]
+/// Error that can happen when parsing a post and compiling it to HTML
+pub struct CompilePostError {
+    pub line: u32,
+    pub column: u32,
+    pub message: String,
+}
+
+pub fn prepare_for_publish<'a>(
+    p: &'a BlogPost<'a>,
+    compiler: &'a PostCompiler<'a>,
+) -> PublishableBlogPost<'a> {
+    let html_document = render_post_page(p, compiler);
+    let metadata = &p.metadata;
+    let filename = match metadata.slug {
+        Some(ref s) => slugify(s),
+        None => slugify(&metadata.title),
+    };
+    let filename = Path::new(&filename).with_extension("html");
+
+    PublishableBlogPost {
+        post: p,
+        filename,
+        rendered_html: html_document,
+    }
+}
+
+pub fn build_blog_post<'a>(
+    content: &str,
+    compiler: &'a PostCompiler<'a>,
+) -> Result<BlogPost<'a>, CompilePostError> {
+    let ast = compiler.to_ast(content);
+
+    let metadata = match parse_frontmatter(ast) {
+        Ok(settings) => settings,
+        Err(msg) => {
+            return Err(CompilePostError {
+                message: msg,
+                line: 10,
+                column: 20,
+            })
+        }
+    };
+
+    Ok(BlogPost {
+        raw_content: content.to_owned(),
+        ast,
+        metadata,
+    })
+}
+
+fn render_post_page<'a>(p: &'a BlogPost<'a>, compiler: &'a PostCompiler<'a>) -> String {
+    let rendered_html = compiler.ast_to_html(p.ast);
+    let base_url = "/home/vini/projects/rust/personal/pageturtle/pageturtle_cli/dist";
+
+    PostTemplate {
+        title: &p.metadata.title,
+        content: &rendered_html,
+        base_url
+    }
+    .render()
+    .unwrap()
+}
+
+pub fn render_index<'a>(posts: &'a Vec<PublishableBlogPost<'a>>) -> String {
+    // TODO: get summary from posts (maybe first AST text nodes) ?
+    // let iter = posts.into_iter();
+
+    let base_url = "/home/vini/projects/rust/personal/pageturtle/pageturtle_cli/dist";
+    IndexTemplate { posts, title: "Welcome to the blog", base_url }
+    .render()
+    .unwrap()
+}
+
+fn parse_frontmatter<'a>(ast: &'a Node<'a, RefCell<Ast>>) -> Result<BlogPostMetadata, String> {
+    use comrak::nodes::NodeValue::*;
+
+    let mut frontmatter: Option<String> = None;
+
+    for node in ast.borrow().traverse() {
+        match node {
+            comrak::arena_tree::NodeEdge::Start(nv) => {
+                if let FrontMatter(s) = &nv.borrow().data.borrow().value {
+                    frontmatter = Some(s.to_owned())
+                }
+            }
+            comrak::arena_tree::NodeEdge::End(_nv) => continue,
+        }
+    }
+
+    match frontmatter {
+        Some(s) => parse_frontmatter_yaml(&s),
+        None => Err("could not find frontmatter section in file".to_owned()),
+    }
+}
+
+fn parse_frontmatter_yaml(s: &str) -> Result<BlogPostMetadata, String> {
+    let unprefixed = unquote_frontmatter(s);
+    match serde_yaml::from_str::<BlogPostMetadata>(&unprefixed) {
+        Ok(settings) => Ok(settings),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+fn unquote_frontmatter(fm: &str) -> String {
+    fm.replace("---", "")
+}
